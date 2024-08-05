6c9d6c8628a8e99e8be01712bfcebfce7d8e8b06
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-08-05 19:27:57 -0300

    services almost done

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,15 +1,23 @@
 import './Home.css'
 import { HeaderHome } from '../HeaderHome/HeaderHome'
-import HomeImage from '../../assets/mid.jpg'
+import HomeImage from '../../assets/homeproject.jpg'
+import { Services } from '../Services/Services'
 
 export const Home = () => {
   return (
     <>
+    <div className='home'>
     <HeaderHome/>
-    <div>
-        
+    <div className='homeimage-info'>
+        <h1>É a hora de construir seu Software conosco</h1>
+        <h2>O que você quer pra hoje? construir um código, deploy do seu projeto, uma colaboração ou até mesmo análise de códigos e projetos </h2>
+        <button className='homeimage-info-btn'>Começar</button>
     </div>
      <img className='homeimage' src={HomeImage} alt="" />
+     </div>
+     <div>
+        <Services/>
+     </div>
     </>
    
   )
---
+++ src/components/Services/Services.tsx
@@ -0,0 +1,70 @@
+import './Services.css'
+import UxDesign from '../../assets/web-design.svg';
+import AppDev from '../../assets/app-development.svg';
+import Coding from '../../assets/coding.svg';
+import Data from '../../assets/data.svg';
+import Analyse from '../../assets/coding-data-development-svgrepo-com.svg';
+
+export const Services = () => {
+    return (
+        <>
+            <div className='services'>
+                <div className='services-title-container'>
+                    <div className='services-title-left'>
+                        <p className='services-title-offer'>O que Nós Temos a Oferecer <span>---</span></p>
+                        <h1 className='services-title'>Temos Exatamente o Serviço que Você Precisa para seu Negócio !</h1>
+                    </div>
+                    <div className='services-title-right'>
+                        <span className='services-title-right-span'></span>
+                        <p className='services-subtitle'>Lorem ipsum, dolor eos numquam. Sit fugiat maiores doloremque vitae quam autem? Excepturi voluptate quas vel.</p>
+                    </div>
+                </div>
+                <div className='services-content-container'>
+                    <div className='services-content'>
+                    <p style={{margin:0,zIndex:1}}>UI/UX Designs Criativos</p><br></br>
+                    <span className='services-content-circlestylebackground'></span>
+                    <span className='services-content-background'></span>
+                        <img className='services-content-icon' id='svg1' src={UxDesign} alt="" />
+                        <p className='p-services-learn-more' style={{marginTop:10}}>Encontrar Mais</p>
+                        
+                    </div>
+                    <div className='services-content'>
+                    <p style={{margin:0,zIndex:1}}>Design e Desenvolvimento de Aplicativos Móveis</p>
+                    <span className='services-content-circlestylebackground'></span>
+                     <span className='services-content-background'></span>
+                        <img className='services-content-icon' id='svg1' src={AppDev} alt="" />
+                        <p className='p-services-learn-more' style={{marginTop:10}}>Encontrar Mais</p>
+                        
+                        
+                    </div>
+                    <div className='services-content'>
+                    <p style={{margin:0,zIndex:1}}>Desenvolvimento de Websites e Portais Web</p>
+                    <span className='services-content-circlestylebackground'></span>
+                     <span className='services-content-background'></span>
+                        <img className='services-content-icon' id='svg1' src={Coding} alt="" />
+                        <p className='p-services-learn-more' style={{marginTop:10}}>Encontrar Mais</p>
+                        
+                        
+                    </div>
+                    <div className='services-content'>
+                    <p style={{margin:0,zIndex:1}}>Análise de Dados e Business Intelligence (BI)</p>
+                    <span className='services-content-circlestylebackground'></span>
+                     <span className='services-content-background'></span>
+                        <img className='services-content-icon' id='svg1' src={Analyse} alt="" />
+                        <p className='p-services-learn-more' style={{marginTop:10}}>Encontrar Mais</p>
+                        
+                    </div>
+                    <div className='services-content'>
+                    <p style={{margin:0,zIndex:1}}>Serviços de Qualidade e Testes de Software</p><br></br>
+                    <span className='services-content-circlestylebackground'></span>
+                     <span className='services-content-background'></span>
+                        <img className='services-content-icon' id='svg1' src={Data} alt="" />
+                        <p className='p-services-learn-more' style={{marginTop:10}}>Encontrar Mais</p>
+                        
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
