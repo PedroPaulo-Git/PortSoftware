4e637c1cc7b4fea210d16f05e78e25fff5a7dde1
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-08-12 18:06:53 -0300

    update home header

--- src/components/HeaderHome/HeaderHome.tsx
+++ src/components/HeaderHome/HeaderHome.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import Logo from '../../assets/PortICON.png'
+import './HeaderHome.css'
 
 export const HeaderHome = () => {
   return (
-    <div></div>
+    <div className='headerhome'>
+      <div className='header'>
+      <img className='headerhome-logo' src={Logo} alt="" />
+      <div className='header-mid'>
+
+
+      </div>
+      <div className='header-right'>
+      <ul>
+        <li>Home</li>
+        <li>Serviços</li>
+        <li>Projetos</li>
+        <li>Contato</li>
+      </ul>
+
+      </div>
+
+      </div>
+    </div>
   )
 }
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -9,23 +9,27 @@ import { Footer } from '../Footer/Footer'
 export const Home = () => {
   return (
     <>
-    <div className='home'>
-    <HeaderHome/>
-    <div className='homeimage-info'>
-        <h1>É a hora de construir seu Software conosco</h1>
-        <h2>O que você quer pra hoje? construir um código, deploy do seu projeto, uma colaboração ou até mesmo análise de códigos e projetos </h2>
-        <button className='homeimage-info-btn'>Começar</button>
-    </div>
-     <img className='homeimage' src={HomeImage} alt="" />
-     </div>
-     <div>
-        <Services/>
-        <Project/>
-        <Contact/>
-        <Footer/>
-     </div>
+      <div className='home'>
+        <HeaderHome />
+        <div className='homeimage-info'>
+          <h1>É a Hora de Construir seu Software Conosco</h1>
+          <h2>O que você quer pra hoje? construir um código, deploy do seu projeto, uma colaboração ou até mesmo análise de códigos e projetos </h2>
+          <div className='homeimage-info-bottom'>
+            <button className='homeimage-info-btn'>Começar</button>
+            
+            </div>
+
+        </div>
+        <img className='homeimage' src={HomeImage} alt="" />
+      </div>
+      <div>
+        <Services />
+        <Project />
+        <Contact />
+        <Footer />
+      </div>
     </>
-   
+
   )
 }
 export default Home
