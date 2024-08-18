6fb4b2bd2b6ee0ddae500da019baa106a92612c8
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-08-18 08:33:51 -0300

    design background done

--- src/components/backgroundDesign/BackgroundDesign.tsx
+++ src/components/backgroundDesign/BackgroundDesign.tsx
@@ -8,8 +8,13 @@ export const BackgroundDesign = () => {
         <img className='background-image' src={Background} alt="" /> 
         <div className='background-container-content' >
             <div className='background-info' >
-
-
+              <h1>Vamos Criar Algo Incrível Juntos</h1>
+              <div className='background-info-p'>
+              <p>Lorem ipsum dolor consequuntur, itaque vel est. Cum itaque vel , itaque vel quasi!
+              </p>
+              <p>optio repudiandae quam vel est. Cum, quasi!</p>
+              </div>
+                <a href="" className='background-info-button'><button className='background-info-button'>Solicitar atendimento</button></a>
             </div>
         </div>
        
