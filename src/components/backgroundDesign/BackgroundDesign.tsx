5bd020a7183b0dc9fe5e8d803ba3859d82c9a02e
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-09-03 09:13:48 -0300

    background design responsive

--- src/components/backgroundDesign/BackgroundDesign.tsx
+++ src/components/backgroundDesign/BackgroundDesign.tsx
@@ -10,9 +10,7 @@ export const BackgroundDesign = () => {
             <div className='background-info' >
               <h1>Vamos Criar Algo Incrível Juntos</h1>
               <div className='background-info-p'>
-              <p>Lorem ipsum dolor consequuntur, itaque vel est. Cum itaque vel , itaque vel quasi!
-              </p>
-              <p>optio repudiandae quam vel est. Cum, quasi!</p>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab quos facilis nisi nulla deleniti vero minus neque porro quo? Libero, doloremque in.</p>
               </div>
                 <a href="" className='background-info-button'><button className='background-info-button'>Solicitar atendimento</button></a>
             </div>
