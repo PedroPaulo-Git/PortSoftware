eacfdcf4804d90e285b30ff92ff34b49e1b02ab4
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-09-07 06:48:08 -0300

    css updates in navbar mobile

--- src/components/NavMobile/NavMobile.tsx
+++ src/components/NavMobile/NavMobile.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoClose } from "react-icons/io5";
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
 import './NavMobile.css'
 
 interface NavMobileProps {
@@ -7,19 +9,34 @@ interface NavMobileProps {
 }
 
 export const NavMobile: React.FC<NavMobileProps> = ({ toggleMenuOff }) => {
+  const [arrowService, setArrowService] = useState(false)
 
+  const toggleArrow = () => {
+    setArrowService(!arrowService);
+  };
   return (
     <div className='NavMobile'>
 
-        <div className='close-icon' onClick={toggleMenuOff}>
-        <IoClose/>
+      <div className='close-icon' onClick={toggleMenuOff}>
+        <IoClose />
       </div>
 
-   
-    <nav className='navmobile-nav'>
+
+      <nav className='navmobile-nav'>
         <ul>
           <li><a href="#home">Home</a></li>
-          <li><a href="#services">Serviços</a> </li>
+          <li className='navbar-li-services'><a href="#services">Serviços</a>  
+           <span onClick={toggleArrow}>
+            {arrowService ? <IoIosArrowDown /> : <IoIosArrowForward />}
+          </span> 
+          </li>
+          {arrowService && (
+          <ul className='submenu' style={{marginTop:-10}}>
+            <li><a href="#services" >Websites</a></li>
+            <li><a href="#services">e-commerce</a></li>
+            <li><a href="#services">landing pages</a></li>
+          </ul>
+        )}
           <li><a href="#projects">Projetos</a></li>
           <li><a href="#contact">Contato</a></li>
           <li><a href="#footer">Sobre Nós</a></li>
