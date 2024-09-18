4b881326cebabc112e275777ba0382ded9ce4a6a
PedroPaulo-Git <pedropaulopyter6@gmail.com>
2024-09-18 13:43:58 -0300

    contato container back end working

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -6,6 +6,27 @@ import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 
 export const Contact = () => {
+    const form = useRef<HTMLFormElement | null>(null);
+
+    const sendEmail = (e: React.FormEvent) => {
+      e.preventDefault();
+
+      if (form.current) {
+        emailjs
+          .sendForm('service_4mwypqm', 'template_sz04wmd', form.current, {
+            publicKey: 'WC8Gsx_d4lEAuQ49g',
+          })
+          .then(
+            () => {
+              console.log('SUCCESS!');
+            },
+            (error) => {
+              console.log('FAILED...', error.text);
+            },
+          );
+      }
+    };
+    
     return (
         <div id='contact' className="contact">
             <img className="contact-background" src={BackgroundContact} alt="" />
@@ -14,17 +35,15 @@ export const Contact = () => {
             <div className="contact-container">
             <span className="sub-title">Contate-nos</span>
             <h1 className="title">Procurando algo para seu negócio?</h1>
-                <form action="#">
+                <form  ref={form} onSubmit={sendEmail}>
+                <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
 
-                    <input placeholder='Seu Nome' type="text" id="name" name="name" required />
-
-
-                    <input placeholder='Seu Email' type="email" id="email" name="email" required />
-
-
-                    <textarea placeholder='Escreva uma Mensagem' id="message" rows={8} name="message" required></textarea>
-
-                    <button type="submit">Enviar Mensagem</button>
                 </form>
 
             </div>
